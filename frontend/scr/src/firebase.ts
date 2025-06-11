f653fedd3819e81163537ccc5d94c0c954b7a24e
alensaruga <alen.saruga01@gmail.com>
2025-06-11 20:49:35 +0200

    feat: update firebase configuration to use environment variables and add emulator support

--- frontend/scr/src/firebase.ts
+++ frontend/scr/src/firebase.ts
@@ -8,21 +8,24 @@ import {
 } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0fy11n1vziFWbBm3iAWy7R_pXwJXFYH0",
-  authDomain: "faasnaloge-b3081.firebaseapp.com",
-  projectId: "faasnaloge-b3081",
-  storageBucket: "faasnaloge-b3081.firebasestorage.app",
-  messagingSenderId: "600936244870",
-  appId: "1:600936244870:web:47599d712df601e9249307",
-  measurementId: "G-0CRJFQM3YE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 
-export const functions = getFunctions(app);
+export const functions = getFunctions(app); 
 
+// Povezava na emulator funkcij
+// Port 5001 je privzeti port za Functions emulator.
+// Drugi parameter (regija) v getFunctions ni potreben, ko uporabljate emulator.
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
   connectFunctionsEmulator(functions, "127.0.0.1", 5001);
 }
@@ -36,8 +39,6 @@ export const confirmReservation = httpsCallable(functions, "confirmReservation")
 export const addCourt = httpsCallable(functions, "addCourt");
 export const isAdmin = httpsCallable(functions, "isAdmin");
 
-
-export const getAllReservationsUrl =
-  (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-    ? `http://127.0.0.1:5001/${firebaseConfig.projectId}/us-central1/getAllReservations`
-    : `https://us-central1-${firebaseConfig.projectId}.cloudfunctions.net/getAllReservations`;
+// URL za onRequest funkcijo, ko deluje lokalno z emulatorjem
+// Uporabite točen URL, ki ga Firebase CLI pokaže ob zagonu emulatorja (običajno v formatu: http://127.0.0.1:5001/YOUR_PROJECT_ID/YOUR_REGION/FUNCTION_NAME)
+export const getAllReservationsUrl = `http://127.0.0.1:5001/faasnaloge-b3081/us-central1/getAllReservations`;
